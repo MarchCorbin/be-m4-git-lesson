be59d48807db44e169aa5e986608df25ff027198
Corbin March <marchcorbin@gmail.com>
2020-10-06 10:49:06 -0600

    Made some more changes

--- config/puma.rb
+++ config/puma.rb
@@ -23,13 +23,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # This directive tells Puma to first boot the application and load code
 
-# process behavior so workers use less memory. If you use this option
-# you need to make sure to reconnect any threads in the `on_worker_boot`
-# block.
-#
-# preload_app!
-
-# If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
 # are forked to prevent connection leakage.
 #
